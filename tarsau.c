9f8defe809a646bbd34b116e869a73e738bd2f47
summ08 <103904421+summ08@users.noreply.github.com>
2026-05-23 02:03:34 +0300

    hata fonksiyonu oluşturuldu

--- tarsau.c
+++ tarsau.c
@@ -1,1 +1,11 @@
+#include "tarsau.h"
+#include <direct.h>
+#ifdef _WIN32
 
+
+
+static void print_hata(const char *filename) {
+   fprintf(stderr, "%s giris dosyasinin formati uyumsuzdur!\n", filename);
+}
+
+  
